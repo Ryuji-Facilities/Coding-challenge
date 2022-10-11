5fd43cbee09e97622187dcf85270bcd86c6699b1
W. Syafiuddin <encrypt0r-x@protonmail.com>
2022-10-11 17:08:46 +0700

    [PYTHON] - Fix for Binary Search Algorithm

--- Python/Binary Search.py
+++ Python/Binary Search.py
@@ -1,19 +1,24 @@
-target = 24
-elements = [6, 20, 21, 24, 30, 32, 80, 85, 90, 101]
+elements = [int(item) for item in input("Enter the list items, separated with space \n>>> ").split()]
+elements.sort()
+target = int(input("Set the number to search \n>>>  "))
 
 def BinarySearch(min_index,max_index):
-    guess_index=(max_index+min_index)//2
-
-    if elements[guess_index]==target:
-        print(target,guess_index)
+    if min_index +1 == max_index:
+        print(f"{target} isn't exist on the List")
         exit()
-    
-    if elements[guess_index]<target:
-        min_index=guess_index
-        BinarySearch(guess_index,max_index)
-    
-    elif elements[guess_index]>target:
-        max_index=guess_index
-        BinarySearch(min_index,guess_index)
+    else:
+        guess_index=(max_index+min_index)//2
 
-BinarySearch(0,len(elements)-1)
+        if elements[guess_index]==target:
+            print(f"{target} found at index {guess_index}")
+            exit()
+
+        if elements[guess_index]<target:
+            min_index=guess_index
+            BinarySearch(guess_index,max_index)
+
+        elif elements[guess_index]>target:
+            max_index=guess_index
+            BinarySearch(min_index,guess_index)
+
+BinarySearch(0,len(elements))
